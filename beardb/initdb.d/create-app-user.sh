3eb181388c55e385d205f6697486bc54015f5695
stephenwu <stephenwu2020@protonmail.com>
2020-08-21 15:30:10 +0800

    init db

--- beardb/initdb.d/create-app-user.sh
+++ beardb/initdb.d/create-app-user.sh
@@ -4,12 +4,14 @@ set -Eeuo pipefail
  
 # Based on mongo/docker-entrypoint.sh
 # https://github.com/docker-library/mongo/blob/master/docker-entrypoint.sh#L303
-if [ "$MONGO_INITDB_USERNAME" ] && [ "$MONGO_INITDB_PASSWORD" ]; then
-    "${mongo[@]}" -u "$MONGO_INITDB_ROOT_USERNAME" -p "$MONGO_INITDB_ROOT_PASSWORD" --authenticationDatabase "$rootAuthDatabase" "$MONGO_INITDB_DATABASE" <<-EOJS
-        db.createUser({
-            user: $(_js_escape "$MONGO_INITDB_USERNAME"),
-            pwd: $(_js_escape "$MONGO_INITDB_PASSWORD"),
-            roles: [ { role: 'readWrite', db: $(_js_escape "$MONGO_INITDB_DATABASE") } ]
-        })
-    EOJS
+if [ "$MONGO_INITDB_ROOT_USERNAME" ] && [ "$MONGO_INITDB_ROOT_PASSWORD" ]; then
+			rootAuthDatabase='admin'
+
+			"${mongo[@]}" "$rootAuthDatabase" <<-EOJS
+				db.createUser({
+					user: $(_js_escape "$MONGO_INITDB_ROOT_USERNAME"),
+					pwd: $(_js_escape "$MONGO_INITDB_ROOT_PASSWORD"),
+					roles: [ { role: 'root', db: $(_js_escape "$rootAuthDatabase") } ]
+				})
+			EOJS
 fi
