54b78bf8ddf5d9f06d41df52d74a1fc3095300d4
stephenwu <stephenwu2020@protonmail.com>
2020-08-22 13:27:34 +0800

    simplify test files

--- bear/test/e2e/conf.go
+++
@@ -1,8 +0,0 @@
-package e2e
-
-var (
-	lenOfLen   = 4
-	lenOfMsgId = 4
-	ip         = "127.0.0.1:3563"
-	//ip = "140.82.11.84:3563"
-)
--- bear/test/e2e/e2e.go
+++ bear/test/e2e/e2e.go
@@ -4,6 +4,13 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+var (
+	lenOfLen   = 4
+	lenOfMsgId = 4
+	ip         = "127.0.0.1:3563"
+	//ip = "140.82.11.84:3563"
+)
+
 func init() {
 	log.SetFormatter(&log.TextFormatter{
 		DisableTimestamp: true,
