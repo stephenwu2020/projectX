9e13f4fab401e26f02bedddad2e2277a81a2e9f8
stephenwu <stephenwu2020@protonmail.com>
2020-08-23 12:50:18 +0800

    comments

--- bear/login/register.go
+++ bear/login/register.go
@@ -16,11 +16,12 @@ func setMsghandler(msg interface{}, handler interface{}) {
 	Module.RegisterChanRPC(reflect.TypeOf(msg), handler)
 }
 
-// event fire at connection establish
+// Here is the place to establish relationship between socket agent and user
 func newAgent(args []interface{}) {
 
 }
 
+// Here release connection between socket and user
 func closeAgent(args []interface{}) {
 
 }
